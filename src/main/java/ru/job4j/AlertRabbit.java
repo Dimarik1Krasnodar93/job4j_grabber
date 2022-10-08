0c6fc857dcac42e6a8ada8bfdf20ecaf1a76b68c
Dimarik <krasnodar93@mail.ru>
2022-10-08 16:52:58 +0300

    1.1. Job c параметрами [#260360 #349408] 08 102ё

--- src/main/java/ru/job4j/AlertRabbit.java
+++ src/main/java/ru/job4j/AlertRabbit.java
@@ -54,7 +54,8 @@ public class AlertRabbit {
         try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
             properties.load(in);
         } catch (Exception ex) {
-            ex.printStackTrace();}
+            ex.printStackTrace();
+        }
         return properties;
     }
 
